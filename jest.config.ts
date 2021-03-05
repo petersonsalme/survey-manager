cac9edb62d430084675d1d7f31d82b50e4eb3e0f
Peterson Vieira Salme <petersonsalme@gmail.com>
2021-03-05 08:26:49 -0300

    Correct regex

--- jest.config.ts
+++ jest.config.ts
@@ -147,7 +147,7 @@ export default {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "**/__tests__/*.test.ts"
+    "**/__tests__/*/*.test.ts"
     // "**/__tests__/**/*.[jt]s?(x)",
     // "**/?(*.)+(spec|test).[tj]s?(x)"
   ],
