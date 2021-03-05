94686ff4f81894700365fdd29fdecc36b8858457
Peterson Vieira Salme <petersonsalme@gmail.com>
2021-03-05 08:29:14 -0300

    Create some integrations tests

---
+++ src/__tests__/integration/Survey.test.ts
@@ -0,0 +1,43 @@
+import request from "supertest";
+import { app } from "../../app";
+import createConnection from '../../database'
+
+describe('Surveys', async () => {
+
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    it('should create new survey', async () => {
+        const response = await request(app).post('/surveys').send({
+            title: 'title',
+            description: 'description'
+        });
+
+        expect(response.status).toBe(201);
+
+        expect(response.body).toHaveProperty('id');
+
+        expect(response.body).toHaveProperty('title');
+        expect(response.body.title).toBe('title');
+
+        expect(response.body).toHaveProperty('description');
+        expect(response.body.description).toBe('description');
+
+        expect(response.body).toHaveProperty('created_at');
+    });
+
+    it('should list surveys', async () => {
+        const response = await request(app).get('/surveys');
+
+        expect(response.status).toBe(200);
+
+        console.log('OPA', response.body);
+
+        expect(response.body.length).toBe(1);
+        expect(response.body[0].title).toBe('title');
+        expect(response.body[0].description).toBe('description');
+    });
+
+});
---
+++ src/__tests__/integration/User.test.ts
@@ -0,0 +1,34 @@
+import request from "supertest";
+import { app } from "../../app";
+import createConnection from '../../database'
+
+describe('Users', async () => {
+
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    it('should create new user', async () => {
+        const response = await request(app)
+        .post('/users')
+        .send({
+            email: 'user@example.com',
+            name: 'User Example'
+        });
+
+        expect(response.status).toBe(201);
+    });
+
+    it('should not create user with duplicate email', async () => {
+        const response = await request(app)
+        .post('/users')
+        .send({
+            email: 'user@example.com',
+            name: 'User Example'
+        });
+
+        expect(response.status).toBe(400);
+    });
+
+});
