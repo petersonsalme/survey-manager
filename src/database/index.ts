118ba1b0f6653bcf530f76c1e72f03bf7d323c4f
Peterson Vieira Salme <petersonsalme@gmail.com>
2021-03-05 08:28:25 -0300

    Extract app creation and configure test database

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,12 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-createConnection();
+export default async (): Promise<Connection> => {
+    const defaultOptions = await getConnectionOptions();
+    const testDatabaseName = "./src/database/database.test.sqlite";
+        
+    const assinedOptions = Object.assign(defaultOptions, { 
+        database: process.env.NODE_ENV === 'test' ? testDatabaseName : defaultOptions.database 
+    });
+
+    return createConnection(assinedOptions);
+}
