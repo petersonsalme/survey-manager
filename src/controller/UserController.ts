6a76e83703085d1c0543f6a0be499bf7e9b1cd12
Peterson Vieira Salme <petersonsalme@gmail.com>
2021-03-05 07:23:49 -0300

    Add CREATED (201) status to POST /users

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -19,7 +19,7 @@ class UserController {
         const user = usersRepository.create({ name, email });
         await usersRepository.save(user);
 
-        return response.json(user);
+        return response.status(201).json(user);
     }
 
 }
