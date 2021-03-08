85b26c0c3e0913e04827372f0f449413408e3bac
Peterson Vieira Salme <petersonsalme@gmail.com>
2021-03-08 16:58:23 -0300

    Add Yup for DTO validation

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,14 +1,25 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repository/UsersRepository";
+import * as yup from 'yup';
 
 class UserController {
 
     async create(request: Request, response: Response) {
         const {name, email} = request.body;
         
-        const usersRepository = getCustomRepository(UsersRepository);
+        const schema = yup.object().shape({
+            name: yup.string().required("Nome é obrigatório"),
+            email: yup.string().email("Email inválido").required("Email é obrigatório")
+        });
 
+        try {
+            await schema.validate(request.body, { abortEarly: false });
+        } catch (error) {
+            return response.status(400).json({ error_list: error.errors });   
+        }
+
+        const usersRepository = getCustomRepository(UsersRepository);
         const userAlreadyExists = await usersRepository.findOne({ email });
         if (userAlreadyExists) {
             return response.status(400).json({
